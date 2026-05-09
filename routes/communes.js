e17a6416a3128cdab50a452dee8c3e87d580b95d
mavillesaine <mavillesaine@hotmail.com>
2026-05-09 22:35:32 +0200

    Update communes.js

--- routes/communes.js
+++ routes/communes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { supabase } = require("../server");
+const { supabase } = require("./supabase");
 
 router.get("/detect", async (req, res) => {
   try {
